e8fae42145ac1145ae19bd450af124565384c417
pendingupdate <157871655+pendingupdate@users.noreply.github.com>
2024-10-17 00:46:38 +0000

    create main class

---
+++ CTA1Opt2.java
@@ -0,0 +1,14 @@
+
+/**
+* CTA1Opt2Module 1 - Critical Thinking Assignment - Option #2:  Implementing a Superclass Employee
+*   Stephanie Peets
+*   Colorado State University Global
+*   CSC372: Computer Programming II
+*   Dr. Douglas Mujeye
+*   October 16, 2024
+*
+*/
+
+public static void main(String[] args) {
+    
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# CSC372
-CSUG Computer Programming II
+# CSC372: Computer Programming II
+## Module 1 - Critical Thinking Assignment - Option #2:  Implementing a Superclass Employee
