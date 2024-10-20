dc4f5d13911e85aef26fa4f166e881d84d362bbf
Steph <157871655+pendingupdate@users.noreply.github.com>
2024-10-19 22:15:41 -0400

    Update Test.java

--- CTA1Opt2/src/cta1opt2/Test.java
+++ CTA1Opt2/src/cta1opt2/Test.java
@@ -3,16 +3,15 @@ package cta1opt2;
 public class Test {
 
 	public static void main(String[] args) {
-
+		// Instance of Employee
 		Employee employee1 = new Employee("Jen", "Johnson", 1);
-		
-		employee1.salary = 65000;
 
+		// Instance of Manager subclass
 		Manager manager1 = new Manager("Ellie", "Smythe", 2, "Accounting");
 
+		// Print Employee summaries
 		employee1.employeeSummary();
 		manager1.employeeSummary();
-
+		System.out.printf("");
 	}
-
 }
