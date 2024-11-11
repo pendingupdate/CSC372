7c043241ed68f8d52ee2bad9f444b1651da2bf1a
Stephanie Peets <stephanie.peets@csuglobal.edu>
2024-11-11 00:15:05 -0500

    add Triangle

---
+++ CriticalThinking/ctaiv/ctaivoptii/src/main/java/csc372/modiv/ctaivoptii/Triangle.java
@@ -0,0 +1,34 @@
+package csc372.modiv.ctaivoptii;
+
+public class Triangle extends Shape {
+    // Attributes
+    private double side1;
+    private double side2;
+    private double side3;
+
+    // Constructor initializes side lengths
+    public Triangle(double side1, double side2, double side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    // Calculate perimeter
+    @Override
+    public double perimeter() {
+        return side1 + side2 + side3;
+    }
+
+    // Calculate area
+    @Override
+    public double area() {
+        double s = perimeter() / 2; // semi-perimeter
+        return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+    }
+
+    // toString method prints out the area and perimeter
+    @Override
+    public String toString() {
+        return "Triangle [Area: " + area() + ", Perimeter: " + perimeter() + "]";
+    }
+}
